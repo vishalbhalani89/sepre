91c001696a9b4004c88d63d0c519a90cb52bbb36
Vishal Bhalani <vishalbhalani89@gmail.com>
2017-05-20 23:06:58 +0200

    removed not required part from post build script

--- buildroot/post-build.sh
+++ buildroot/post-build.sh
@@ -10,12 +10,5 @@ if [ -e ${TARGET_DIR}/etc/inittab ]; then
 tty1::respawn:/sbin/getty -L  tty1 0 vt100 # HDMI console' ${TARGET_DIR}/etc/inittab
 fi
 
-cp -rf /home/vishal/buildroot/board/raspberrypi/post_build/interfaces ${TARGET_DIR}/etc/network/
-rm -rf ${TARGET_DIR}/etc/resolv.conf
-cp -rf /home/vishal/buildroot/board/raspberrypi/post_build/resolv.conf ${TARGET_DIR}/etc/resolv.conf
 cp -rf /home/vishal/buildroot/board/raspberrypi/post_build/sshd_config ${TARGET_DIR}/etc/ssh/
-cp -rf /home/vishal/buildroot/board/raspberrypi/post_build/wpa_supplicant.conf ${TARGET_DIR}/etc/
-cp -rf /home/vishal/buildroot/board/raspberrypi/post_build/S100sepre ${TARGET_DIR}/etc/init.d/
-cp -rf /home/vishal/buildroot/board/raspberrypi/post_build/dhcpd.conf ${TARGET_DIR}/etc/
-cat /home/vishal/buildroot/board/raspberrypi/post_build/environment >> ${TARGET_DIR}/etc/profile
-
+cp -rf /home/vishal/buildroot/board/raspberrypi/post_build/hosts ${TARGET_DIR}/etc/
